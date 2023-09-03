eba0d346e2d44b8c94dded09d22bd26daacd4612
itsvarsharma <101050108+itsvarsharma@users.noreply.github.com>
2023-09-03 14:05:01 +0530

    Create 9_JumpGame.java

---
+++ 9_JumpGame.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(max>=i)max=Math.max(max,i+nums[i]);
+            if(max>=nums.length-1)return true;
+        }
+        return false;
+    }
+}
